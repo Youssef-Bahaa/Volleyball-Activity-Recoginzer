119c211c1170fb3155508740c909bf78cdd308bc
Youssef-Bahaa <youssefbahaa755@gmail.com>
2026-05-09 14:02:16 +0300

    .

--- src/models/B6/B6_Temporal.py
+++ src/models/B6/B6_Temporal.py
@@ -5,9 +5,7 @@ class Temporal_Group_Classifier(nn.Module):
     def __init__(self, input_size=2048, hidden_size=512, num_classes=8):
         super().__init__()
 
-        self.lstm = nn.LSTM(hidden_size=512,
-                            input_size=2048,
-                            batch_first=True)
+        self.lstm = nn.LSTM(hidden_size=512,input_size=2048,batch_first=True)
 
         self.fc = nn.Sequential(
             nn.Linear(hidden_size, 128),
