be72220889a3d71ae55583d799401b821fb2cd26
Youssef-Bahaa <youssefbahaa755@gmail.com>
2026-05-09 13:23:08 +0300

    Implemented B5 loader

---
+++ src/dataset/DataLoader/B5_loader.py
@@ -0,0 +1,141 @@
+import os
+import pickle
+from PIL import Image
+import torch
+from torch.utils.data import Dataset, DataLoader, Subset
+
+from src.dataset.transforms import train_transform, val_transform, test_transform
+from src.dataset.utils import activity2id
+from src.dataset.boxinfo import BoxInfo
+from src.dataset.utils import activity2id, TransformSubset, filter_by_ids
+import sys
+import src.dataset.boxinfo as _boxinfo_mod
+
+sys.modules.setdefault('boxinfo', _boxinfo_mod)
+
+
+class _Unpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if name == 'BoxInfo':
+            return BoxInfo
+        return super().find_class(module, name)
+
+
+class PersonTemporalDataset(Dataset):
+    def __init__(self, image_root, annot_path, transform=None):
+        self.samples = []
+        self.transform = transform
+
+        with open(annot_path, 'rb') as f:
+            data = pickle.load(f)
+
+        for video_id in os.listdir(image_root):
+            video_dir = os.path.join(image_root, video_id)
+            if not os.path.isdir(video_dir):
+                continue
+
+            for clip_id in os.listdir(video_dir):
+                clip_dir = os.path.join(video_dir, clip_id)
+                if not os.path.isdir(clip_dir):
+                    continue
+
+                try:
+                    clip_info = data[video_id][clip_id]
+                    frame_boxes = clip_info['frame_boxes_dct']
+                except KeyError:
+                    continue
+
+                sorted_frames = sorted(frame_boxes.keys())
+
+                img_paths = [
+                    os.path.join(clip_dir, f"{fid}.jpg")
+                    for fid in sorted_frames
+                    if os.path.exists(os.path.join(clip_dir, f"{fid}.jpg"))
+                ]
+
+                if not img_paths:
+                    continue
+
+                self.samples.append((video_id, clip_id, img_paths, sorted_frames, frame_boxes))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        video_id, clip_id, img_paths, frames, frame_boxes = self.samples[idx]
+
+        num_persons = 12
+        persons_data = [[] for _ in range(num_persons)]
+        labels = [None] * num_persons
+
+        for t, img_path in enumerate(img_paths):
+            fid = frames[t]
+            boxes = frame_boxes[fid]
+
+            img = Image.open(img_path).convert('RGB')
+
+            box_dict = {b.player_ID: b for b in boxes}
+
+            for pid in range(num_persons):
+                box_info = box_dict.get(pid)
+
+                if box_info is None:
+                    dummy = self.transform(img) if self.transform else img
+                    persons_data[pid].append(torch.zeros_like(dummy))
+
+                else:
+                    if labels[pid] is None:
+                        labels[pid] = activity2id(box_info.action.lower())
+
+                    x1, y1, x2, y2 = box_info.box
+                    crop = img.crop((x1, y1, x2, y2))
+
+                    crop = self.transform(crop) if self.transform else crop
+                    persons_data[pid].append(crop)
+
+        labels = [l if l is not None else 0 for l in labels]
+
+        video_tensor = torch.stack([torch.stack(p) for p in persons_data])  # (N, T, C, H, W)
+
+        labels = torch.tensor(labels, dtype=torch.long)
+
+        return video_id, clip_id, video_tensor, labels
+
+
+def build_loaders(cfg):
+    data_cfg = cfg["data"]
+    training_cfg = cfg["training"]
+
+    train_dataset = PersonTemporalDataset(
+        data_cfg["videos_path"],
+        data_cfg["annot_path"],
+        transform=train_transform
+    )
+
+    val_dataset = PersonTemporalDataset(
+        data_cfg["videos_path"],
+        data_cfg["annot_path"],
+        transform=val_transform
+    )
+
+    test_dataset = PersonTemporalDataset(
+        data_cfg["videos_path"],
+        data_cfg["annot_path"],
+        transform=test_transform
+    )
+
+    train_dataset = filter_by_ids(train_dataset, data_cfg["video_splits"]["train"])
+    val_dataset = filter_by_ids(val_dataset, data_cfg["video_splits"]["validation"])
+    test_dataset = filter_by_ids(test_dataset, data_cfg["video_splits"]["test"])
+
+    loader_kwargs = {
+        "batch_size": training_cfg["batch_size"],
+        "num_workers": training_cfg.get("num_workers", 4),
+        "pin_memory": training_cfg.get("pin_memory", True),
+    }
+
+    train_loader = DataLoader(train_dataset, shuffle=True, **loader_kwargs)
+    val_loader = DataLoader(val_dataset, shuffle=False, **loader_kwargs)
+    test_loader = DataLoader(test_dataset, shuffle=False, **loader_kwargs)
+
+    return train_loader, val_loader, test_loader
