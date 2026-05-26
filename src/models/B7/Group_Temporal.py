00adac0b71a2e6eabbce50306845445a3303d42d
Youssef-Bahaa <youssefbahaa755@gmail.com>
2026-05-26 13:23:20 +0300

    Implemented Group model for B7

---
+++ src/models/B7/Group_Temporal.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+
+class GroupActivityB6(nn.Module):
+    def __init__(self, player_model, hidden_size = 512, num_classes=8):
+        super(GroupActivityB6, self).__init__()
+
+        base = player_model.module if hasattr(player_model, 'module') else player_model
+
+        self.resnet50 = base.backbone
+        self.lstm1 = base.lstm
+
+        for param in self.resnet50.parameters():
+            param.requires_grad = False
+
+        for param in self.lstm1.parameters():
+            param.requires_grad = False
+
+        self.pool = nn.AdaptiveMaxPool2d((1, 2048))
+
+        self.lstm2 = nn.LSTM(
+            input_size=2048,
+            hidden_size=hidden_size,
+            num_layers=1,
+            batch_first=True
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(2048, 2048),
+            nn.BatchNorm1d(2048),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.7),
+
+            nn.Linear(2048, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.6),
+
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.5),
+
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.4),
+
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.3),
+
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, x):
+        b, n, t, c, h, w = x.shape
+        x = x.view(b * n * t, c, h, w)
+        x = self.resnet50(x)
+
+        x = x.view(b * t, n, -1)
+        out , _ = self.lstm1(x)
+
+        x = torch.cat([x, out] , dim=2)
+        x = x.contiguous()
+
+        x = x.view(b * t, n, -1)
+        x = self.pool(x)
+
+
+        x = x.view(b, t, -1)
+        x, (_, _) = self.lstm2(x)
+        x = x [:, -1, :]
+
+        return self.fc(x)
+
+
+
